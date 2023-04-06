ab0e58dbe18a5a13356c35eb07c796bdbfd74133
RadinOstovari <ro341@bath.ac.uk>
2023-04-06 11:47:14 +0100

    random scene

--- Temp/Assets/SceneChanger.cs
+++ Temp/Assets/SceneChanger.cs
@@ -7,15 +7,23 @@ using System.Linq;
 
 public class SceneChanger : MonoBehaviour
 {
-    public int nextSceneIndex; // the index of the next scene to load
+    //public int nextSceneIndex; // the index of the next scene to load
     //public string otherScriptName; // the name of the script with the array you want to check
     //ToggleArray ToggleArray;
-    int[] myArray;
+    //int[] myArray;
+    List<int> savedList = new List<int>();
     
     void Start()
     {
-        string myArrayString = PlayerPrefs.GetString("MyArray");
-        myArray = myArrayString.Split(',').Select(int.Parse).ToArray();
+        //string savedJsonString = PlayerPrefs.GetString("MyList");
+        string intString = PlayerPrefs.GetString("MyList");
+        //List<int> originalList = JsonUtility.FromJson<List<int>>(savedJsonString);
+        List<int> originalList = intString.Split(',').Select(int.Parse).ToList();
+        savedList.AddRange(originalList);
+        //int check = originalList.Count;
+        //Debug.Log(check);
+        /*string myArrayString = PlayerPrefs.GetString("MyArray");
+        myArray = myArrayString.Split(',').Select(int.Parse).ToArray();*/
         /*GameObject otherObject = GameObject.Find("VR Mask Quad 1");
 
         // Get a reference to the script on the GameObject
@@ -31,7 +39,15 @@ public class SceneChanger : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            while (true)
+            int randomIndex = UnityEngine.Random.Range(0, savedList.Count);
+            //Debug.Log(randomIndex);
+            int nextSceneIndex = savedList[randomIndex];
+            //Debug.Log(nextSceneIndex);
+            savedList.RemoveAt(randomIndex);
+            //string updatedJsonList = JsonUtility.ToJson(savedList);
+            string updatedList = string.Join(",", savedList.Select(i => i.ToString()).ToArray());
+            PlayerPrefs.SetString("myList", updatedList);
+            /*while (true)
             {
                 int index = Array.IndexOf(myArray, nextSceneIndex);
                 if(index == -1)
@@ -43,7 +59,7 @@ public class SceneChanger : MonoBehaviour
                     //SceneManager.LoadScene(nextSceneIndex);
                     break;
                 }
-            }
+            }*/
             SceneManager.LoadScene(nextSceneIndex);
         }
     }
--- Temp/Assets/ToggleArray.cs
+++ Temp/Assets/ToggleArray.cs
@@ -17,16 +17,16 @@ public class ToggleArray : MonoBehaviour
     public Toggle toggle6;
     public Toggle toggle7;
     public Toggle toggle8;
-    public int nextSceneIndex;
+    //public int nextSceneIndex;
     public TMP_InputField inputField;
     //private int index;
 
-    public int[] myArray;
+    //public int[] myArray;
+    List<int> scenesList = new List<int>();
 
     void Start()
     {
-        myArray = new int[16];
-        // Set initial values of array
+        /*myArray = new int[16];
         myArray[0] = 0;
         myArray[1] = 0;
         myArray[2] = 0;
@@ -42,26 +42,27 @@ public class ToggleArray : MonoBehaviour
         myArray[12] = 0;
         myArray[13] = 0;
         myArray[14] = 0;
-        myArray[15] = 0;
+        myArray[15] = 0;*/
 
         // Add listeners to toggles
-        toggle1.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
+        /*toggle1.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
         toggle2.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
         toggle3.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
         toggle4.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
         toggle5.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
         toggle6.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
         toggle7.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
-        toggle8.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });
+        toggle8.onValueChanged.AddListener(delegate { OnToggleValueChanged(); });*/
     }
 
-    void OnToggleValueChanged()
+    /*void OnToggleValueChanged()
     {
-        // Update array based on toggle values
         if (toggle1.isOn)
         {
             myArray[0] = 1;
             myArray[1] = 2;
+            scenesList.Add(1);
+            scenesList.Add(2);
         }
         else if (!toggle1.isOn)
         {
@@ -72,6 +73,8 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[2] = 3;
             myArray[3] = 4;
+            scenesList.Add(3);
+            scenesList.Add(4);
         }
         else if (!toggle2.isOn)
         {
@@ -82,6 +85,8 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[4] = 5;
             myArray[5] = 6;
+            scenesList.Add(5);
+            scenesList.Add(6);
         }
         else if (!toggle3.isOn)
         {
@@ -92,6 +97,8 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[6] = 7;
             myArray[7] = 8;
+            scenesList.Add(7);
+            scenesList.Add(8);
         }
         else if (!toggle4.isOn)
         {
@@ -102,6 +109,8 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[8] = 9;
             myArray[9] = 10;
+            scenesList.Add(9);
+            scenesList.Add(10);
         }
         else if (!toggle5.isOn)
         {
@@ -112,6 +121,8 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[10] = 11;
             myArray[11] = 12;
+            scenesList.Add(11);
+            scenesList.Add(12);
         }
         else if (!toggle6.isOn)
         {
@@ -122,6 +133,8 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[12] = 13;
             myArray[13] = 14;
+            scenesList.Add(13);
+            scenesList.Add(14);
         }
         else if (!toggle7.isOn)
         {
@@ -132,71 +145,122 @@ public class ToggleArray : MonoBehaviour
         {
             myArray[14] = 15;
             myArray[15] = 16;
+            scenesList.Add(15);
+            scenesList.Add(16);
         }
         else if (!toggle8.isOn)
         {
             myArray[14] = 0;
             myArray[15] = 0;
         }
-        /*if (toggle1.isOn && toggle2.isOn && toggle3.isOn)
-        {
-            myArray[0] = 1;
-            myArray[1] = 1;
-            myArray[2] = 1;
-        }
-        else if (toggle1.isOn && toggle2.isOn && !toggle3.isOn)
-        {
-            myArray[0] = 1;
-            myArray[1] = 1;
-            myArray[2] = 0;
-        }
-        else if (toggle1.isOn && !toggle2.isOn && toggle3.isOn)
-        {
-            myArray[0] = 1;
-            myArray[1] = 0;
-            myArray[2] = 1;
-        }
-        else if (!toggle1.isOn && toggle2.isOn && toggle3.isOn)
-        {
-            myArray[0] = 0;
-            myArray[1] = 1;
-            myArray[2] = 1;
-        }
-        else if (toggle1.isOn && !toggle2.isOn && !toggle3.isOn)
-        {
-            myArray[0] = 1;
-            myArray[1] = 0;
-            myArray[2] = 0;
-        }
-        else if (!toggle1.isOn && toggle2.isOn && !toggle3.isOn)
-        {
-            myArray[0] = 0;
-            myArray[1] = 1;
-            myArray[2] = 0;
-        }
-        else if (!toggle1.isOn && !toggle2.isOn && toggle3.isOn)
-        {
-            myArray[0] = 0;
-            myArray[1] = 0;
-            myArray[2] = 1;
-        }
-        else
-        {
-            myArray[0] = 0;
-            myArray[1] = 0;
-            myArray[2] = 0;
-        }*/
-    }
+    }*/
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
+            if (toggle1.isOn)
+            {
+                //myArray[0] = 1;
+                //myArray[1] = 2;
+                scenesList.Add(1);
+                scenesList.Add(2);
+            }
+            /*else if (!toggle1.isOn)
+            {
+                myArray[0] = 0;
+                myArray[1] = 0;
+            }*/
+            if (toggle2.isOn)
+            {
+                //myArray[2] = 3;
+                //myArray[3] = 4;
+                scenesList.Add(3);
+                scenesList.Add(4);
+            }
+            /*else if (!toggle2.isOn)
+            {
+                myArray[2] = 0;
+                myArray[3] = 0;
+            }*/
+            if (toggle3.isOn)
+            {
+                //myArray[4] = 5;
+                //myArray[5] = 6;
+                scenesList.Add(5);
+                scenesList.Add(6);
+            }
+            /*else if (!toggle3.isOn)
+            {
+                myArray[4] = 0;
+                myArray[5] = 0;
+            }*/
+            if (toggle4.isOn)
+            {
+                //myArray[6] = 7;
+                //myArray[7] = 8;
+                scenesList.Add(7);
+                scenesList.Add(8);
+            }
+            /*else if (!toggle4.isOn)
+            {
+                myArray[6] = 0;
+                myArray[7] = 0;
+            }*/
+            if (toggle5.isOn)
+            {
+                //myArray[8] = 9;
+                //myArray[9] = 10;
+                scenesList.Add(9);
+                scenesList.Add(10);
+            }
+            /*else if (!toggle5.isOn)
+            {
+                myArray[8] = 0;
+                myArray[9] = 0;
+            }*/
+            if (toggle6.isOn)
+            {
+                //myArray[10] = 11;
+                //myArray[11] = 12;
+                scenesList.Add(11);
+                scenesList.Add(12);
+            }
+            /*else if (!toggle6.isOn)
+            {
+                myArray[10] = 0;
+                myArray[11] = 0;
+            }*/
+            if (toggle7.isOn)
+            {
+                //myArray[12] = 13;
+                //myArray[13] = 14;
+                scenesList.Add(13);
+                scenesList.Add(14);
+            }
+            /*else if (!toggle7.isOn)
+            {
+                myArray[12] = 0;
+                myArray[13] = 0;
+            }*/
+            if (toggle8.isOn)
+            {
+                //myArray[14] = 15;
+                //myArray[15] = 16;
+                scenesList.Add(15);
+                scenesList.Add(16);
+            }
+            /*else if (!toggle8.isOn)
+            {
+                myArray[14] = 0;
+                myArray[15] = 0;
+            }*/
             //int index = Array.IndexOf(myArray, nextSceneIndex);
             //Debug.Log(index);
-            while (true)
+            /*while (true)
             {
                 //Debug.Log(nextSceneIndex);
-                int index = Array.IndexOf(myArray, nextSceneIndex);
+                //int index = Array.IndexOf(myArray, nextSceneIndex);
+                int index = scenesList.IndexOf(nextSceneIndex);
                 if(index == -1)
                 {
                     nextSceneIndex++;
@@ -204,14 +268,22 @@ public class ToggleArray : MonoBehaviour
                 else
                 {
                     //SceneManager.LoadScene(nextSceneIndex);
+                    scenesList.RemoveAt(index);
                     break;
                 }
-            }
+            }*/
+            int randomIndex = UnityEngine.Random.Range(0, scenesList.Count);
+            int nextSceneIndex = scenesList[randomIndex];
+            scenesList.RemoveAt(randomIndex);
             int result = 0;
             int.TryParse(inputField.text, out result);
             PlayerPrefs.SetInt("MyInt", result);
-            string myArrayString = string.Join(",", myArray.Select(i => i.ToString()).ToArray());
-            PlayerPrefs.SetString("MyArray", myArrayString);
+            /*string myArrayString = string.Join(",", myArray.Select(i => i.ToString()).ToArray());
+            PlayerPrefs.SetString("MyArray", myArrayString);*/
+            //string jsonString = JsonUtility.ToJson(scenesList);
+            string intString = string.Join(",", scenesList.Select(i => i.ToString()).ToArray());
+            //Debug.Log(intString);
+            PlayerPrefs.SetString("MyList", intString);
             SceneManager.LoadScene(nextSceneIndex);
         }
     }
